355180bdb33dd7c6610d137760c247ca0287166b
julesGoullee <julesgoullee@gmail.com>
2020-09-04 03:58:52 +0700

    addr

--- src/config/index.js
+++ src/config/index.js
@@ -1,7 +1,7 @@
 const Config = {
   contracts: {
     // goldenAnanas: '0x9aEAC5B72b511e9C746D912Bf2143F7BadfE58ee', // local
-    goldenAnanas: '0x61BEd144331f20B577AACE81EE974f80BBf17169' // ropsten
+    goldenAnanas: '0x25Ebb87dd290E0653409dE9A30fA4f42A81A5EC4' // ropsten
     // goldenAnanas: '0x6232f85F7E36657903F1A0a2b52392654A2a094D' // mainnet
   },
   minContribution: 0.05, // in eth
