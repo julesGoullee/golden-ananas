98503672024303adb74b1578e00151375ed7c28b
julesGoullee <julesgoullee@gmail.com>
2020-09-08 17:51:21 +0700

    welcomePopup

--- src/modules/welcomePopup.ts
+++ src/modules/welcomePopup.ts
@@ -3,17 +3,23 @@ import { PromptStyles, ButtonStyles } from "../../node_modules/@dcl/ui-utils/uti
 
 export default () => {
   const prompt = new ui.CustomPrompt(PromptStyles.DARKLARGE, 500, 600)
-  prompt.addIcon(`images/goldAnanas.png`, 0, 50, 256, 256, {
+  prompt.addIcon(`images/goldAnanas.png`, 0, 110, 200, 200, {
     sourceWidth: 520,
     sourceHeight: 520
   })
-  prompt.addText('Welcome to the Golden Ananas Challenge!', 0, 230, Color4.White(), 20)
-  const content = prompt.addText(`            Do you think you're ready ?! 
-         
-The challenge is to activate the button above.
-Jump on the cloud, activate the button down
-here is a good start
-`, -140, -170)
+  prompt.addText('Welcome to the Golden Ananas Challenge!', 0, 260, Color4.White(), 20)
+  const content = prompt.addText(`First of all, this game is designed to be played at
+the first person, click on V to switch views.
+
+Your goal is to jump on the path of chained clouds
+as quickly as possible.
+
+So ready?
+Get on the first cloud and, then click on the button
+to start playing.
+
+We hope you will have fun!
+`, -140, -180)
   content.text.hTextAlign = 'left'
   prompt.addButton(
     `Go!`,
